c057b90078f48a307b4080d1bae3f0532049f9ee
Thiago T. Moura <rpj134@gmail.com>
2022-06-03 13:39:47 -0300

    Class 04

--- main.c
+++ main.c
@@ -5,8 +5,15 @@ int main(void){
     //print game header
     printf("*****************************************\n");
     printf("***   Welcome to the Guessing Game !  ***\n");
-    printf("*****************************************\n");
+    printf("*****************************************\n\n\n");
 
     int sec_num = 42;   //secret number to hit 
+
+    int attempt;        //variable that holds the attempt
+
+    printf("What is your attempt?\n");
+    scanf("%d", &attempt);
+
+
 
 }
