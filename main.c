c30bfdfa260e90ed923c35299665a7d9c384d8c0
Thiago T. Moura <rpj134@gmail.com>
2022-06-03 15:10:33 -0300

    Class 07

--- main.c
+++ main.c
@@ -10,28 +10,31 @@ int main(void)
 
     int sec_num = 42;   //secret number to hit 
 
-    int attempt;        //variable that holds the attempt
-
-    printf("What is your attempt?\n");
-    scanf("%d", &attempt);
-
-    if(attempt == sec_num)  //if the attempt is equal the secret number, you're right
-    {     
-        printf("Congratulations! You got it right!\n");
-        printf("Play again, you're a great player\n");
-    }
-    else   //esle, you missed
+    int guess;        //variable that holds the guess
+    
+    for(int i=0; i < 3; i++)  // have 3 attempts to guess the secret number 
     {
-        if(attempt < sec_num)   //returns if the attempt was less than or greater than the secret number
-        {
-            printf("You're attempt was less than the secret number!\n");
-        }
-        else
-        {
-            printf("You're attempt was greater that the secret number!\n");
-        }
+        printf("What is your guess?\n");
+        scanf("%d", &guess);
 
+        if(guess == sec_num)  //if the guess is equal the secret number, you're right
+        {     
+            printf("Congratulations! You got it right!\n");
+            printf("Play again, you're a great player\n");
+            break;    //break the for loop 
+        }
+        else   //esle, you missed
+        {
+            if(guess < sec_num)   //returns if the guess was less than or greater than the secret number
+            {
+                printf("You're guess was less than the secret number!\n");
+            }
+            else
+            {
+                printf("You're guess was greater that the secret number!\n");
+            }
+
+        }
     }
-
-
+    printf("End Game!\n");
 }
